47f0ee716bc7da1173ac5737592811ca19a03c79
page <page@seventh77.com>
2020-10-31 08:45:29 +0800

    新增产品图片等

--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ function loadUIInit() {
         var item = { url: unpackImg[i], type: Loader.IMAGE };
         Resources.push(item);
     }
+    for (var i = 1; i <= 26; i++) {
+        var item = { url: "images/product/"+i+".jpg", type: Loader.IMAGE };
+        Resources.push(item);
+    }
     Laya.loader.load(Resources, laya.utils.Handler.create(this, loadComplete), laya.utils.Handler.create(this, loadPerUpdate, null, false));
 }
 
@@ -49,6 +53,7 @@ function loadComplete() {
     iPortalPage.init();
     iBigSencePage.init();
     iSmallSencePage.init();
+    iProductPage.init();
 
     iLoadingPage.hide();
     iChosePage.show();
---
+++ src/pages/product.js
@@ -0,0 +1,42 @@
+var productPage = function(){
+    var _self = this;
+    var page;
+
+    /**
+     * 初始化
+     */
+    _self.init = function () {
+        uiInit();
+        eventInit();
+    }
+
+    /**
+     * 事件初始化
+     */
+    function eventInit() {
+        
+    }
+
+    /**
+     * 显示
+     */
+    _self.show = function(){
+        
+    }
+
+    /**
+     * 隐藏
+     */
+    _self.hide = function(){
+        
+    }
+
+    /**
+     * ui初始化
+     */
+    function uiInit() {
+        
+    }
+}
+
+iProductPage = new productPage();
